c76642d40b5774ccab5aa00e6ecd295dd0ede74b
Jonathan Washburn <jrwashburn@gmail.com>
2021-02-28 02:16:53 -0500

    rename K to minimum confirmations; clean up environment file to remove unused values

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,16 @@ import { getPayouts } from "./core/payouts";
 
 async function main() {
   // TODO: Fail if any required values missing from .env
-  const stakingPoolKey: string = process.env.MINA_KEY || "";
+  const stakingPoolPublicKey: string = process.env.POOL_PUBLIC_KEY || "";
   const globalSlotStart = Number(process.env.GLOBAL_SLOT_START);
   const minHeight = Number(process.env.MIN_HEIGHT); // This can be the last known payout or this could be a starting date
   // TODO: Get K programatically? Required dependency on a running node
-  const k = Number(process.env.K);
+  const minimumConfirmations = Number(process.env.MIN_CONFIRMATIONS);
   const slotsPerEpoch = Number(process.env.SLOTS_PER_EPOCH);
   const commissionRate = Number(process.env.COMMISSION_RATE);
 
-  console.log(`This script will payout from blocks ${minHeight} to the current max height minus k: ${k}`);
-  const payouts = await getPayouts(stakingPoolKey, minHeight, globalSlotStart, k, slotsPerEpoch, commissionRate);
+  console.log(`This script will payout from block ${minHeight} to the current max height minus required confirmations of: ${minimumConfirmations}`);
+  const payouts = await getPayouts(stakingPoolPublicKey, minHeight, globalSlotStart, minimumConfirmations, slotsPerEpoch, commissionRate);
   console.log(payouts);
 }
 
