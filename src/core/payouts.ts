1fcfa85777eb554c6d828756d52e3bbf45675367
Jonathan Washburn <jrwashburn@gmail.com>
2021-02-27 23:33:40 -0500

    skipping empty blocks

--- src/core/payouts.ts
+++ src/core/payouts.ts
@@ -25,6 +25,7 @@ export async function getPayouts(stakingPoolKey: string, minHeight: number, glob
     blocksIncluded.push(block.blockheight);
 
     // TODO: check for no coinbase - if no coinbase, skip the block?
+<<<<<<< Updated upstream
     let sumEffectivePoolStakes = 0;
     let effectivePoolStakes: { [key: string]: number } = {};
 
@@ -93,6 +94,81 @@ export async function getPayouts(stakingPoolKey: string, minHeight: number, glob
       };
       //TODO: Store data 
     });
+=======
+    if ( typeof(block.coinbase) === 'undefined' || block.coinbase == 0 ) {
+      console.log(`skipping block ${block.blockheight} because there is no coinbase`);
+    } else {
+      
+      let sumEffectivePoolStakes = 0;
+      let effectivePoolStakes: { [key: string]: number } = {};
+
+      // Determine the supercharged weighting for the block
+      let txFees = block.usercommandtransactionfees || 0;
+      let superchargedWeighting = 1 + 1 / (1 + txFees / block.coinbase);
+
+      // What are the rewards for the block
+      let totalRewards = block.blockpayoutamount
+      let totalFees = commissionRate * totalRewards;
+
+      console.log(`txfees: ${txFees}, superchargedWeighting: ${superchargedWeighting}, totalRewards: ${totalRewards}, totalFees: ${totalFees}, coinbase: ${block.coinbase}`);
+
+      allBlocksTotalRewards += totalRewards;
+      allBlocksTotalFees += totalFees;
+
+      // #TODO this should match the fee transfer to the coinbase receiver. Add an assert it can't be larger.
+      // #if "feeTransfer" not in b["transactions"]:
+      // #    # Just coinbase so we can't pay out more than the coinbase. We also may have an orphaned block.
+      // #    #assert total_rewards <= int(b["transactions"]["coinbase"])
+      // #else:
+      // #    # There were some fee transfers so let's _really_ make sure we don't pay out more than we received
+
+      // Loop through our list of delegates to determine the weighting per block
+
+      // TODO: need to handle rounding issues 
+
+      stakers.forEach((staker: any) => {
+        let superchargedContribution =
+          (superchargedWeighting - 1) * staker.timedWeighting + 1;
+        let effectiveStake = staker.stakingBalance * superchargedContribution;
+        effectivePoolStakes[staker.publicKey] = effectiveStake;
+        sumEffectivePoolStakes += effectiveStake;
+      });
+
+      // Sense check the effective pool stakes must be at least equal to total_staking_balance and less than 2x
+      //TODO: assert total_staking_balance <= sum_effective_pool_stakes <= 2 * total_staking_balance
+
+      // Determine the effective pool weighting based on sum of effective stakes
+      stakers.forEach((staker: any) => {
+        let effectivePoolWeighting =
+          effectivePoolStakes[staker.publicKey] / sumEffectivePoolStakes;
+
+          // This must be less than 1 or we have a major issue
+        //TODO: assert effective_pool_weighting <= 1
+
+        let blockTotal = Math.round(
+          (totalRewards - totalFees) * effectivePoolWeighting
+        );
+        staker.total += blockTotal;
+
+        // Store this data in a structured format for later querying and for the payment script, handled seperately
+        let storePayout = {
+          publicKey: staker.publicKey,
+          blockHeight: block.blockheight,
+          stateHash: block.statehash,
+          effectivePoolWeighting: effectivePoolWeighting,
+          effectivePoolStakes: effectivePoolStakes[staker.publicKey],
+          stakingBalance: staker.stakingBalance,
+          sumEffectivePoolStakes: sumEffectivePoolStakes,
+          superchargedWeighting: superchargedWeighting,
+          dateTime: block.blockdatetime,
+          coinbase: block.coinbase,
+          totalRewards: totalRewards,
+          payout: blockTotal,
+        };
+        //TODO: Store data 
+      });
+    }
+>>>>>>> Stashed changes
   });
 
   // ################################################################
