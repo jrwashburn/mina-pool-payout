5038096a6906fddb6fd4f7d09438664f4ac5803f
Jonathan Washburn <jrwashburn@gmail.com>
2021-03-05 17:57:30 -0500

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,16 @@ import { getStakes } from "./core/stakes";
 import { getBlocks, getLatestHeight } from "./core/queries";
 
 // TODO: Error handling
-// TODO: Check mina vs nano calcs
 // TODO: Add parameter to run read-only vs. write which would persist max height processed so it is not re-processed in future
+// TODO: Fail if any required values missing from .env
+const stakingPoolPublicKey: string = process.env.POOL_PUBLIC_KEY || "";
+const globalSlotStart = Number(process.env.GLOBAL_SLOT_START) || 0;
+const minimumHeight = Number(process.env.MIN_HEIGHT); // This can be the last known payout or this could be a starting date
+const minimumConfirmations = Number(process.env.MIN_CONFIRMATIONS) || 0;
+const slotsPerEpoch = Number(process.env.SLOTS_PER_EPOCH);
+const commissionRate = Number(process.env.COMMISSION_RATE);
 
 async function main() {
-  // TODO: Fail if any required values missing from .env
-  const stakingPoolPublicKey: string = process.env.POOL_PUBLIC_KEY || "";
-  const globalSlotStart = Number(process.env.GLOBAL_SLOT_START);
-  const minimumHeight = Number(process.env.MIN_HEIGHT); // This can be the last known payout or this could be a starting date
-  const minimumConfirmations = Number(process.env.MIN_CONFIRMATIONS);
   // MAX_HEIGHT is optional - if not provided, set to max
   let configuredMaximum = 0;
   if (typeof (process.env.MAX_HEIGHT) === 'undefined') {
@@ -20,9 +21,8 @@ async function main() {
   } else {
     configuredMaximum = Number(process.env.MAX_HEIGHT);
   }
-  const slotsPerEpoch = Number(process.env.SLOTS_PER_EPOCH);
-  const commissionRate = Number(process.env.COMMISSION_RATE);
 
+  // get current maximum block height from database and determine what max block height for this run will be
   let maximumHeight = await determineLastBlockHeightToProcess(configuredMaximum, minimumConfirmations);
 
   console.log(`This script will payout from block ${minimumHeight} to maximum height ${maximumHeight}`);
@@ -35,9 +35,10 @@ async function main() {
   // get the blocks from archive db
   const blocks = await getBlocks(stakingPoolPublicKey, minimumHeight, maximumHeight);
 
+  // run the payout calculation for those blocks
   let [payouts, storePayout, blocksIncluded, allBlocksTotalRewards, allBlocksTotalPoolFees, totalPayout] = await getPayouts(blocks, stakers, totalStake, commissionRate);
 
-  // Checking total results
+  // Output total results and transaction files for input to next process, details file for audit log
   console.log(`We won these blocks: ${blocksIncluded}`);
   console.log(`We are paying out based on total rewards of ${allBlocksTotalRewards} nanomina in this window.`);
   console.log(`That is ${allBlocksTotalRewards / 1000000000} mina`);
