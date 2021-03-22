8db0b97b288cb0c5008c5389832f17c1d8fc4554
Jonathan Washburn <jrwashburn@gmail.com>
2021-03-21 21:55:51 -0400

    removing ephemeral key from env loader

--- src/index.ts
+++ src/index.ts
@@ -26,10 +26,6 @@ async function main() {
   const slotsPerEpoch = Number(process.env.SLOTS_PER_EPOCH) || 7140;
   const commissionRate = Number(process.env.COMMISSION_RATE) || 0.05;
   const payorSendTransactionFee = (Number(process.env.SEND_TRANSACTION_FEE) || 0) * 1000000000;
-  let generateEphemeralSenderKey = false;
-  if (typeof (process.env.SEND_EPHEMERAL_KEY) === 'string' && process.env.SEND_EPHEMERAL_KEY.toLowerCase() == 'true') {
-    generateEphemeralSenderKey = true;
-  };
   let senderKeys: keypair = {
     privateKey: process.env.SEND_PRIVATE_KEY || "",
     publicKey: process.env.SEND_PUBLIC_KEY || ""
