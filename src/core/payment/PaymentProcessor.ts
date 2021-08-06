e5e1b87b0401bd50c32e122ded9cfafe2471e8e9
Juan Romaina <otro34@hotmail.com>
2021-08-06 01:50:08 -0500

    #68 calculate and output the pool revenue from a given payout

--- src/core/payment/PaymentProcessor.ts
+++ src/core/payment/PaymentProcessor.ts
@@ -1,10 +1,11 @@
-import { IPaymentBuilder, IPaymentProcessor as IPaymentProcessor  } from "./Model"
-import { PayoutTransaction } from "../payoutCalculator/Model";
+import { IPaymentBuilder, IPaymentProcessor as IPaymentProcessor, PaymentProcess  } from "./Model"
+import { PayoutDetails, PayoutTransaction } from "../payoutCalculator/Model";
 import { PaymentConfiguration } from "../../configuration/Model";
 import { ConfigurationManager } from "../../configuration/ConfigurationManager";
 import { inject, injectable } from "inversify";
 import TYPES from "../../composition/Types";
 import { ISender, ITransactionBuilder, ITransactionProcessor } from "../transaction/Model";
+import { Block } from "../dataProvider/dataprovider-types";
 
 @injectable()
 export class PaymentProcessor implements IPaymentProcessor {
@@ -42,12 +43,60 @@ export class PaymentProcessor implements IPaymentProcessor {
             await this.transactionProcessor.write(transactions, configuration, paymentProcess) 
 
             await this.sender.send(configuration, transactions, paymentProcess)
+
+            await this.printTotalResults(paymentProcess)
             
         } else {
             //TODO: Use a custom error class
             throw new Error ('Unkown Data Source')
         }
         
+    }
+
+    private async printTotalResults(paymentProcess: PaymentProcess) : Promise<void> {
+
+        const getTotalsFromBlocks = (blocks: Block[]) => {
+            let coinbasesum = 0
+            let feetransferfromcoinbasesum = 0
+            let usercommandtransactionfeessum = 0
+            blocks.forEach (block => {
+                coinbasesum += block.coinbase ?? 0
+                feetransferfromcoinbasesum += block.feetransferfromcoinbase ?? 0
+                usercommandtransactionfeessum += block.usercommandtransactionfees
+            })
+
+            return {coinbasesum, feetransferfromcoinbasesum, usercommandtransactionfeessum}
+        }
+
+        const getTotalPayouts = (transactions: PayoutTransaction[]) => {
+            let amountsum = 0
+            let feesum = 0
+
+            transactions.forEach(transaction => {
+                amountsum += transaction.amount ?? 0
+                feesum += transaction.fee ?? 0
+            })
+
+            return {amountsum,feesum}
+        }
+
+        console.log('------------------- Payout Results ------------------')
+
+        const {coinbasesum, feetransferfromcoinbasesum, usercommandtransactionfeessum} = getTotalsFromBlocks(paymentProcess.blocks)
+
+        const netcoinbasereceived = coinbasesum - feetransferfromcoinbasesum - usercommandtransactionfeessum
+
+        const {amountsum,feesum} = getTotalPayouts(paymentProcess.payouts)
+
+        console.log(`Total Coin Base Generated: ${coinbasesum}`)
+        console.log(`Total User Transaction Fees Generated: ${usercommandtransactionfeessum}`)
+        console.log(`Total Snark Fees Paid: ${feetransferfromcoinbasesum}`)
+        console.log(`Net Coinbase Received: ${netcoinbasereceived}`)
+        console.log(`Total Payouts Sent: ${amountsum}`)
+        console.log(`Total Payout Transaction Fees: ${feesum}`)
+        console.log(`Net MINA to Pool Operator: ${netcoinbasereceived - amountsum - feesum}`)
+        
+        console.log('------------------- Payout Results ------------------')
     }
 
     private async calculateTotalPayoutFundsNeeded(transactions: PayoutTransaction[]) : Promise<number> {
@@ -69,3 +118,4 @@ export class PaymentProcessor implements IPaymentProcessor {
         return true
         }
  }
+
