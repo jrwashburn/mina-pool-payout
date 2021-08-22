5db5fbb6f06ddd924c9d18b0d1f147aacbc8e7d5
Jonathan Washburn <jrwashburn@gmail.com>
2021-08-21 21:50:56 -0400

    clarifying summary messages, correcting net pool calculation

--- src/core/payment/PaymentSummarizer.ts
+++ src/core/payment/PaymentSummarizer.ts
@@ -6,7 +6,7 @@ import { ISummarizer, PaymentProcess } from "./Model";
 @injectable()
 export class PaymentSummarizer implements ISummarizer<PaymentProcess> {
     async calculateTotals(base: PaymentProcess) : Promise<void> {
-        
+
         const getTotalsFromBlocks = (blocks: Block[]) => {
             let coinbasesum = 0
             let feetransferfromcoinbasesum = 0
@@ -34,32 +34,33 @@ export class PaymentSummarizer implements ISummarizer<PaymentProcess> {
 
         const {coinbasesum, feetransferfromcoinbasesum, usercommandtransactionfeessum} = getTotalsFromBlocks(base.blocks)
 
-        const netcoinbasereceived = coinbasesum - feetransferfromcoinbasesum - usercommandtransactionfeessum
+        const netcoinbasereceived = coinbasesum - feetransferfromcoinbasesum + usercommandtransactionfeessum
 
         const {amountsum,feesum} = getTotalPayouts(base.payouts)
 
         base.totals = {
-            coinBaseSum: coinbasesum,
-            feeTransferFromCoinBaseSum: feetransferfromcoinbasesum,
-            userCommandTransactionFeeSum: usercommandtransactionfeessum,
-            netCoinBaseReceived: coinbasesum - feetransferfromcoinbasesum + usercommandtransactionfeessum,
-            payoutAmountsSum: amountsum,
-            payoutFeesSum: feesum,
-            netMinaToPoolOperator: netcoinbasereceived - amountsum - feesum
+            coinBaseSum: coinbasesum / 1000000000,
+            feeTransferFromCoinBaseSum: feetransferfromcoinbasesum / 1000000000,
+            userCommandTransactionFeeSum: usercommandtransactionfeessum / 1000000000,
+            netCoinBaseReceived: ( coinbasesum - feetransferfromcoinbasesum + usercommandtransactionfeessum ) / 1000000000,
+            payoutAmountsSum: amountsum / 1000000000,
+            payoutFeesSum: feesum / 1000000000,
+            netMinaToPoolOperator: ( netcoinbasereceived - amountsum - feesum ) / 1000000000
         }
     }
 
     async printTotals(base: PaymentProcess): Promise<void> {
-        console.log('------------------- Summary && Totals ------------------')
+        console.log('------------------- Summary & Totals ------------------')
+        console.log('Calculations based on entire pool - ignoring substitutions and exclusions.')
 
         console.log(`Total Coin Base Generated: ${base.totals?.coinBaseSum}`)
-        console.log(`Total User Transaction Fees Generated: ${base.totals?.userCommandTransactionFeeSum}`)
-        console.log(`Total Snark Fees Paid: ${base.totals?.feeTransferFromCoinBaseSum}`)
+        console.log(`User Transaction Fees Generated (net of snark fees): ${base.totals?.userCommandTransactionFeeSum}`)
+        console.log(`Total Snark Fees Paid From Coinbase: ${base.totals?.feeTransferFromCoinBaseSum}`)
         console.log(`Net Coinbase Received: ${base.totals?.netCoinBaseReceived}`)
-        console.log(`Total Payouts Sent: ${base.totals?.payoutAmountsSum}`)
+        console.log(`Total Amounts Due To Stakers: ${base.totals?.payoutAmountsSum}`)
         console.log(`Total Payout Transaction Fees: ${base.totals?.payoutFeesSum}`)
-        console.log(`Net MINA to Pool Operator: ${base.totals?.netMinaToPoolOperator}`)
-        
+        console.log(`Net MINA to Pool Operator (before send transaction fees): ${base.totals?.netMinaToPoolOperator}`)
+
         console.log('------------------- Summary && Totals ------------------')
 
     }
