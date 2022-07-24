6dca50224af4d1b9e8b325c73cf2dd806357c73f
Jonathan Washburn <jrwashburn@gmail.com>
2022-07-23 20:13:41 -0400

    adding ability to resend saved payment.gqlfiles

---
+++ src/utils/resend-payment.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { gql } from '@apollo/client/core';
+import fs from 'fs';
+import path, { resolve } from 'path';
+import { sendPaymentGraphQL } from '../infrastructure/graphql-pay';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function resendSignedPaymentFromFile(args: any): Promise<void> {
+    const dir = path.join(__dirname, '/../data/');
+    const fromNonce = args.fromNonce;
+    const fileList = fs.readdirSync(dir).filter((f) => {
+        if (f.substring(f.indexOf('.')) === '.gql') {
+            if (Number(f.substring(0, f.indexOf('.'))) >= Number(fromNonce)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+
+    for (const f of fileList) {
+        console.log(`processing file ${f}`);
+        await sendSignedPaymentFromFile(path.join(dir, f));
+        await delay5s();
+    }
+}
+
+async function sendSignedPaymentFromFile(filename: string): Promise<void> {
+    const fileData = fs.readFileSync(filename);
+    const paymentDoc = gql`
+        ${fileData.toString()}
+    `;
+    try {
+        await sendPaymentGraphQL(paymentDoc, {});
+        console.log(`Sent payments for nonce: ${filename} again.`);
+    } catch (error) {
+        console.log(`Failed to send payments for nonce ${filename}`);
+        throw error;
+    }
+}
+
+function delay5s() {
+    console.log('wiating 5s');
+    return new Promise((d) => setTimeout(d, 5000));
+}
