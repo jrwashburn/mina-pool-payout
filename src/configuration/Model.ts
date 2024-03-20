04c270322f44e02dffc0368672f5393e2b13048b
Jonathan Washburn <jrwashburn@gmail.com>
2024-03-20 16:17:41 -0600

    switch to mina-signer

--- src/configuration/Model.ts
+++ src/configuration/Model.ts
@@ -1,32 +1,30 @@
-import { keypair } from '@o1labs/client-sdk';
-
 export interface PaymentConfiguration {
-    blockDataSource: string;
-    bpKeyMd5Hash: string;
-    burnAddress: string;
-    burnRatesByPublicKey: KeyedRate;
-    commissionRatesByPublicKey: KeyedRate;
-    configuredMaximum: number;
-    defaultCommissionRate: number;
-    epoch: number;
-    fork: number;
-    investorsCommissionRate: number;
-    mfCommissionRate: number;
-    minimumConfirmations: number;
-    minimumHeight: number;
-    o1CommissionRate: number;
-    payoutHash: string;
-    payoutMemo: string;
-    payorSendTransactionFee: number;
-    payoutThreshold: number;
-    senderKeys: keypair;
-    slotsInEpoch: number;
-    stakingPoolPublicKey: string;
-    verbose: boolean;
+  blockDataSource: string;
+  bpKeyMd5Hash: string;
+  burnAddress: string;
+  burnRatesByPublicKey: KeyedRate;
+  commissionRatesByPublicKey: KeyedRate;
+  configuredMaximum: number;
+  defaultCommissionRate: number;
+  epoch: number;
+  fork: number;
+  investorsCommissionRate: number;
+  mfCommissionRate: number;
+  minimumConfirmations: number;
+  minimumHeight: number;
+  o1CommissionRate: number;
+  payoutHash: string;
+  payoutMemo: string;
+  payorSendTransactionFee: number;
+  payoutThreshold: number;
+  senderKeys: { publicKey: string, privateKey: string };
+  slotsInEpoch: number;
+  stakingPoolPublicKey: string;
+  verbose: boolean;
 }
 
 export interface KeyedRate {
-    [publicKey: string]: { rate: number };
+  [publicKey: string]: { rate: number };
 }
 
 const baseUrl = process.env.PAYOUT_DATA_PROVIDER_API_ENDPOINT;
