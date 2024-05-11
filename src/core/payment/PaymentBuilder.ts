e60423650d0d12ef7bfe2b7a30e558176203d46b
Jonathan Washburn <jrwashburn@gmail.com>
2024-05-11 13:29:44 -0400

    sort blocks from data provider to make comparisons easier and have hashes match

--- src/core/payment/PaymentBuilder.ts
+++ src/core/payment/PaymentBuilder.ts
@@ -56,6 +56,7 @@ export class PaymentBuilder implements IPaymentBuilder {
     console.log(`Processing mina pool payout for block producer key: ${stakingPoolPublicKey}`);
 
     const blocks: Block[] = await blockProvider.getBlocks(stakingPoolPublicKey, minimumHeight, maximumHeight);
+    blocks.sort((a, b) => a.blockheight - b.blockheight);
 
     const payoutTransactions: PayoutTransaction[] = [];
 
@@ -71,7 +72,6 @@ export class PaymentBuilder implements IPaymentBuilder {
         console.log(`### Calculating payouts for ledger ${ledgerHash}`);
 
         const ledger = await stakesProvider.getStakes(ledgerHash, stakingPoolPublicKey);
-
         console.log(`The pool total staking balance is ${ledger.totalStakingBalance}`);
 
         const ledgerBlocks = blocks.filter((x) => x.stakingledgerhash == ledgerHash);
