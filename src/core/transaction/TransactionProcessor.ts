31ce3db4524ced5874d4fa5b5802184f136be564
Jonathan Washburn <jrwashburn@gmail.com>
2024-06-09 13:00:52 -0400

    RangeError: Invalid string length Fixes #144 Changed from JSON.stringify to processing streams for file writer and sha1 calculation

--- src/core/transaction/TransactionProcessor.ts
+++ src/core/transaction/TransactionProcessor.ts
@@ -4,38 +4,53 @@ import { inject, injectable } from 'inversify';
 import TYPES from '../../composition/Types';
 import { ITransactionProcessor } from './Model';
 import { PaymentProcess } from '../payment/Model';
+import { createWriteStream } from 'fs';
 
 @injectable()
 export class TransactionProcessor implements ITransactionProcessor {
   private fileWriter: IFileWriter;
+  private _payoutTransactionsFileName: string = '';
+  private _payoutDetailsFileName: string = '';
 
   constructor(@inject(TYPES.IFileWriter) fileWriter: IFileWriter) {
     this.fileWriter = fileWriter;
   }
+
+  get payoutTransactionsFileName(): string {
+    return this._payoutTransactionsFileName;
+  }
+
+  get payoutDetailsFileName(): string {
+    return this._payoutDetailsFileName;
+  }
+
   async write(config: PaymentConfiguration, paymentProcess: PaymentProcess): Promise<void> {
     const runDateTime = new Date();
 
-    const { payoutDetails, maximumHeight, totalPayoutFundsNeeded, payoutTransactions } = paymentProcess;
-
+    const { payoutDetails, maximumHeight, payoutTransactions } = paymentProcess;
     const { minimumHeight } = config;
-
-    const payoutTransactionsFileName = this.generateOutputFileName(
+    this._payoutTransactionsFileName = this.generateOutputFileName(
       'payout_transactions',
       runDateTime,
       minimumHeight,
       maximumHeight,
     );
 
-    this.fileWriter.write(payoutTransactionsFileName, JSON.stringify(payoutTransactions));
+    //this.fileWriter.write(payoutTransactionsFileName, JSON.stringify(payoutTransactions));
+    const payoutTransactionsStream = createWriteStream(this._payoutTransactionsFileName);
+    payoutTransactions.forEach(transaction => payoutTransactionsStream.write(JSON.stringify(transaction)));
+    payoutTransactionsStream.end();
 
-    const payoutDetailsFileName = this.generateOutputFileName(
+    this._payoutDetailsFileName = this.generateOutputFileName(
       'payout_details',
       runDateTime,
       minimumHeight,
       maximumHeight,
     );
-
-    this.fileWriter.write(payoutDetailsFileName, JSON.stringify(payoutDetails));
+    //this.fileWriter.write(payoutDetailsFileName, JSON.stringify(payoutDetails));
+    const payoutDetailsStream = createWriteStream(this._payoutDetailsFileName);
+    payoutDetails.forEach(detail => payoutDetailsStream.write(JSON.stringify(detail)));
+    payoutDetailsStream.end();
   }
 
   private generateOutputFileName(
@@ -51,3 +66,4 @@ export class TransactionProcessor implements ITransactionProcessor {
     return d.toISOString().replace(/\D/g, '');
   }
 }
+
